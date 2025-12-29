e92f66fb0d8491c65ee9c8834a873fbf8fec2e2d
Can Ali Ateş <97811011+canatess@users.noreply.github.com>
2025-12-29 23:54:26 +0300

    data uploaded
    Removed docstring for data loading and preprocessing utilities.

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,5 +1,3 @@
-"""Data loading and preprocessing utilities for CUB-200-2011 dataset."""
-
 from .dataset import CUB200Dataset, load_class_names, stratified_split, build_dataloaders
 from .transforms import build_transforms
 
